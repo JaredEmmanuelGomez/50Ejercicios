6c9f792789072a97d3e3ef636da424c27e835e70
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-07 14:28:19 -0800

    Update Ejercicio20.s

--- Ejercicio20.s
+++ Ejercicio20.s
@@ -3,3 +3,12 @@
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador
+
+
+/*
+def busqueda_lineal(arreglo, elemento):
+    for i in range(len(arreglo)):
+        if arreglo[i] == elemento:
+            return i
+    return -1
+*/

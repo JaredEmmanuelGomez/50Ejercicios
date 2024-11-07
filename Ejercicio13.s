41434d41721c626860597e1fe34a3fd2e13dd3c5
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-07 14:11:33 -0800

    Update Ejercicio13.s

--- Ejercicio13.s
+++ Ejercicio13.s
@@ -1,5 +1,10 @@
 # Lenguajes de Interfaz - Actividad 13
-# 
+# Convertir decimal a binario
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador
+
+/*
+def decimal_a_binario(decimal):
+    return bin(decimal)[2:]
+*/

77a1da1caf16cbbcad83a56f89a1d1af03fed16f
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-07 14:06:34 -0800

    Update Ejercicio11.s

--- Ejercicio11.s
+++ Ejercicio11.s
@@ -1,5 +1,10 @@
 # Lenguajes de Interfaz - Actividad 11
-# 
+# Invertir una cadena
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador
+
+/*
+def invertir_cadena(cadena):
+    return cadena[::-1]
+*/

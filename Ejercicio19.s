a85f7c807e758a1ccb71baa3c2ed8741d88c118d
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-07 14:29:22 -0800

    Update Ejercicio19.s

--- Ejercicio19.s
+++ Ejercicio19.s
@@ -1,5 +1,11 @@
 # Lenguajes de Interfaz - Actividad 19
-# Invertir los elementos de un arreglo
+# Suma de elementos en un arreglo
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador
+
+/*
+def suma_arreglo(arreglo):
+    return sum(arreglo)
+*/
+

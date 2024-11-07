81413701f87a9197236d698b3d85607d087fdedc
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-07 14:16:51 -0800

    Update Ejercicio17.s

--- Ejercicio17.s
+++ Ejercicio17.s
@@ -1,5 +1,5 @@
 # Lenguajes de Interfaz - Actividad 17
-# 
+# Suma de los N primeros números naturales
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador

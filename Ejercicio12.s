b51e34d2a0dfd3a58d340d96ad64e232dff31700
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-07 14:23:05 -0800

    Update Ejercicio12.s

--- Ejercicio12.s
+++ Ejercicio12.s
@@ -1,5 +1,5 @@
 # Lenguajes de Interfaz - Actividad 12
-# Verificar si una cadena es palíndromo
+# Convertir binario a decimal
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador

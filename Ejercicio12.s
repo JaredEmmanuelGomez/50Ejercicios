22b3f8f7e5e3cdd4bd0582928a0fcf95e1d896b9
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-07 14:08:16 -0800

    Update Ejercicio12.s

--- Ejercicio12.s
+++ Ejercicio12.s
@@ -1,5 +1,11 @@
 # Lenguajes de Interfaz - Actividad 12
-# 
+# Verificar si una cadena es palíndromo
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador
+
+/*
+def es_palindromo(cadena):
+    cadena = cadena.replace(" ", "").lower()
+    return cadena == cadena[::-1]
+*/

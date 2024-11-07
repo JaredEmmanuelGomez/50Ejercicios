5efcb38cd3359cd3327dbcd7dc48892978d922e7
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-07 14:13:00 -0800

    Update Ejercicio14.s

--- Ejercicio14.s
+++ Ejercicio14.s
@@ -1,5 +1,9 @@
 # Lenguajes de Interfaz - Actividad 14
-# 
+# Conversión de decimal a hexadecimal
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador
+
+/*
+
+*/

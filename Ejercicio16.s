9f4ed80a94e3dd62f7bdfebba5b52a388976c127
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-07 14:26:47 -0800

    Update Ejercicio16.s

--- Ejercicio16.s
+++ Ejercicio16.s
@@ -3,3 +3,12 @@
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador
+
+/*
+def factorial(n):
+    resultado = 1
+    for i in range(1, n + 1):
+        resultado *= i
+    return resultado
+*/
+

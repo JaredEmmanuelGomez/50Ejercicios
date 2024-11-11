7103fc34687235ed9b688e7fab1424f01c5e2927
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:14:38 -0800

    Create Ejercicio46.s

---
+++ Ejercicio46.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 46
+# Verificar si un número es Armstrong
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Aritmética y flujo de control

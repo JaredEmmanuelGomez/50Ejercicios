72c0dc3d37c54494f3946abd7589447c915de769
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 19:42:31 -0800

    Update Ejercicio40.s

--- Ejercicio40.s
+++ Ejercicio40.s
@@ -4,3 +4,13 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Manipulación de arreglos
+
+# -------------------------------------------
+/*
+def rotate_array(arr, k, direction="left"):
+    k %= len(arr)
+    if direction == "left":
+        return arr[k:] + arr[:k]
+    return arr[-k:] + arr[:-k]
+*/
+# ------------------------------------------

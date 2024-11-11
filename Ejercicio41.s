f51cd24781cab57a3a323125fb9d42971d3651c5
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 19:41:59 -0800

    Update Ejercicio41.s

--- Ejercicio41.s
+++ Ejercicio41.s
@@ -4,3 +4,12 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Comparación en arreglos
+
+# -----------------------------------------------------------
+/*
+def second_largest(arr):
+    unique_arr = list(set(arr))
+    unique_arr.sort()
+    return unique_arr[-2] if len(unique_arr) >= 2 else None
+*/
+# -----------------------------------------------------------

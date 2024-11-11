ccf63875167121c621939c4a2d8a010b3ac14141
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 18:28:18 -0800

    Update Ejercicio48.s

--- Ejercicio48.s
+++ Ejercicio48.s
@@ -4,3 +4,14 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Detección de desbordamiento
+
+# ----------------------------------------------
+/*
+def check_overflow_addition(a, b):
+    try:
+        result = a + b
+        return result
+    except OverflowError:
+        return "Overflow detected"
+*/
+# --------------------------------------------

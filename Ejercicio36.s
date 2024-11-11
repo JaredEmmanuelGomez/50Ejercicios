0b615c85d4ec02c7b4ca5353891f4c1f4c122d4c
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 16:54:54 -0800

    Create Ejercicio36.s

---
+++ Ejercicio36.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 36
+# Máximo Común Divisor (MCD)
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Aritmética, algoritmo de Euclides

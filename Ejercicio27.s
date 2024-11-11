8f2bcc1c03ea03207470b43c7b09bdbc655d89cd
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:04:12 -0800

    Update Ejercicio27.s

--- Ejercicio27.s
+++ Ejercicio27.s
@@ -7,7 +7,30 @@
 
 # -------------------------------------------------------------------------------
 /*
-def add_matrices(A, B):
-    return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
 */
 # ---------------------------------------------------------------------------------

aea1e76a3c802855e7f76907ed5342622466eebf
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:09:46 -0800

    Create Actividad40.s

---
+++ Actividad40.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 40
+# Rotación de un arreglo (izquierda/derecha)
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Manipulación de arreglos

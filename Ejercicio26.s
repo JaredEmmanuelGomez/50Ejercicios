0104134131814d127f2211fcc4d1718bf00c2349
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 16:39:26 -0800

    Create Ejercicio26.s

---
+++ Ejercicio26.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 26
+# Ordenamiento por selección
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Algoritmos de ordenamiento

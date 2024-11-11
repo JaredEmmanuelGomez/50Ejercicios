630a9651fe2960bb593eecff5f71965ade7455f3
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 16:50:44 -0800

    Create Ejercicio32.s

---
+++ Ejercicio32.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 32
+# Operaciones AND, OR, XOR a nivel de bits
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Operaciones a nivel de bits

ce106c1a05bb378e6fae39eff8df4389bbd9ef00
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 16:44:22 -0800

    Create Ejercicio29.s

---
+++ Ejercicio29.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 29
+# Multiplicación de matrices
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Bucles anidados, operaciones en memoria

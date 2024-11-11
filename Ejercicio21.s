eb8b2d33b168091daee1d60ea004637e55476fde
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:08:02 -0800

    Update Ejercicio21.s

--- Ejercicio21.s
+++ Ejercicio21.s
@@ -5,3 +5,12 @@
 # Python y Ensamblador
 # Bucles y manejo de memoria
 
+# -------------------------------
+/*
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=' ')
+        a, b = b, a + b
+*/
+# -------------------------------

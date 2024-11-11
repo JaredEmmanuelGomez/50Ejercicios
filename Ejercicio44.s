ee0f95c2f80901b729576d8601c87a89d51de6f9
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:13:19 -0800

    Create Ejercicio44.s

---
+++ Ejercicio44.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 44
+# Calculadora simple (Suma, Resta, Multiplicación, División)
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Aritmética y flujo de control

499e553a028df06c525dfd619a75e8713566967c
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 16:42:20 -0800

    Create Ejercicio28.s

---
+++ Ejercicio28.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 28
+# Suma de matrices
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Operaciones con arreglos 2D

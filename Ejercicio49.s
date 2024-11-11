297f953c13e1fe66afc36318020aafa055b52c4a
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:18:38 -0800

    Update Ejercicio49.s

--- Ejercicio49.s
+++ Ejercicio49.s
@@ -1,6 +1,6 @@
 # Lenguajes de Interfaz - Actividad 49
-# Invertir los elementos de un arreglo
+# Medir el tiempo de ejecución de una función
 # Alumno: Gómez Aguilar Jared Emmanuel  
 # Número de Control: 22210309  
 # Python y Ensamblador
-# Medir el tiempo de ejecución de una función
+# Operaciones con temporizadores

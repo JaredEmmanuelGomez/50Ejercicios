4af2dee86233244421590521d9640606809eb24f
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 19:41:32 -0800

    Update Ejercicio42.s

--- Ejercicio42.s
+++ Ejercicio42.s
@@ -4,3 +4,20 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Comparación en arreglos
+
+# -------------------------------------------
+/*
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop() if self.stack else None
+
+    def peek(self):
+        return self.stack[-1] if self.stack else None
+*/
+# -----------------------------------------

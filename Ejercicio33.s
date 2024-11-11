9d3f5e0992ae2390ee9a241bfa548cc9f3d61f26
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:52:07 -0800

    Update Ejercicio33.s

--- Ejercicio33.s
+++ Ejercicio33.s
@@ -11,3 +11,24 @@ def bit_shifts(n, shift):
     return n << shift, n >> shift
 */
 # ----------------------------
+
+// Gómez Aguilar Jared Emmanuel
+// 22210309
+.global bit_shifts
+.text
+
+// x0: número n
+// x1: cantidad de desplazamiento (shift)
+// Retorna en x0 el desplazamiento a la izquierda
+// Retorna en x1 el desplazamiento a la derecha
+bit_shifts:
+    // Guardar el número original
+    mov x2, x0
+    
+    // Realizar desplazamiento a la izquierda
+    lsl x0, x0, x1
+    
+    // Realizar desplazamiento a la derecha usando el número original
+    lsr x1, x2, x1
+    
+    ret

8da77ed1facd14ccd1c691dd84a9e5b7ffe57555
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:15:13 -0800

    Create Ejercicio47.s

---
+++ Ejercicio47.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 46
+# Encontrar prefijo común más largo en cadenas
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Comparación de cadenas

11ffbc60864f49fd087ba4efd702d620dff60d3d
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 16:45:13 -0800

    Create Ejercicio30.s

---
+++ Ejercicio30.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 30
+# Transposición de una matriz
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Manipulación de arreglos

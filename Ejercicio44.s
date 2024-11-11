8ad1be4076af63caea830422e5a246b279d0a68c
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 19:39:38 -0800

    Update Ejercicio44.s

--- Ejercicio44.s
+++ Ejercicio44.s
@@ -4,3 +4,18 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Aritmética y flujo de control
+
+# ----------------------------------------------------
+/*
+def calculator(a, b, operation):
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        return a / b if b != 0 else "Undefined"
+
+*/
+# -----------------------------------------------------

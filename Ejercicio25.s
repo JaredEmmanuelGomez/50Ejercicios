c60a7c90794cdd79df98685117f1f008053ce1be
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:05:12 -0800

    Update Ejercicio25.s

--- Ejercicio25.s
+++ Ejercicio25.s
@@ -4,3 +4,14 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Algoritmos de ordenamiento
+
+# -------------------------------------------------------
+/*
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+*/
+# -------------------------------------------------------

e5c91a38ed8a1bbd182b801a04334168b0e97fa2
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:05:44 -0800

    Update Ejercicio24.s

--- Ejercicio24.s
+++ Ejercicio24.s
@@ -4,3 +4,20 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Recursión y saltos
+
+# ---------------------------------------
+/*
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+*/
+# -------------------------------------

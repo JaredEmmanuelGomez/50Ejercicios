fecb9aa458b97b98f45fb557fc0682f98d0ca573
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:10:16 -0800

    Create Ejercicio41.s

---
+++ Ejercicio41.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 41
+# Encontrar el segundo elemento más grande
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Comparación en arreglos

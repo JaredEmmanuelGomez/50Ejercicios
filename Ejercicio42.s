301abd5592c5ba54b147214717d5e02a7c6855d2
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:11:50 -0800

    Create Ejercicio42.s

---
+++ Ejercicio42.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 42
+# Implementar una pila usando un arreglo
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Comparación en arreglos

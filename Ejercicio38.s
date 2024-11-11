9fa515db80464373dd012dc8890f56487a9de285
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:54:58 -0800

    Update Ejercicio38.s

--- Ejercicio38.s
+++ Ejercicio38.s
@@ -11,3 +11,23 @@ def power(x, n):
     return x ** n
 */
 # -------------------
+
+// Gómez Aguilar Jared Emmanuel
+// 22210309
+.global power
+.text
+
+// x0: base x
+// x1: exponente n
+power:
+    mov x2, #1          // resultado
+    
+power_loop:
+    cbz x1, power_end   // si exponente es 0, terminar
+    mul x2, x2, x0      // multiplicar resultado por base
+    sub x1, x1, #1      // decrementar exponente
+    b power_loop
+    
+power_end:
+    mov x0, x2          // mover resultado al registro de retorno
+    ret

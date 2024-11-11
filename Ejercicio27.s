4debd4dfbdea5869e875a41808e7ddc99afe9e8f
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 16:40:47 -0800

    Create Ejercicio27.s

---
+++ Ejercicio27.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 27
+# Ordenamiento por mezcla (Merge Sort)
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Recursión y manejo de memoria

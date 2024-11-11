335078b2b783c8bcb43749cacc77cd93134f27a2
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:14:03 -0800

    Create Ejercicio45.s

---
+++ Ejercicio45.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 45
+# Generación de números aleatorios (con semilla)
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Generación de números pseudoaleatorios

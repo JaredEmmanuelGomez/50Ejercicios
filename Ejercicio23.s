5cd418d8980af5f3193baa52d2c4beea6cbeeb3f
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 16:35:26 -0800

    Create Ejercicio23.s

---
+++ Ejercicio23.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 23
+# Encontrar el mínimo en un arreglo
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Recorrido de arreglos

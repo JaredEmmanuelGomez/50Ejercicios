b5369096c612b637249bf5d52a0bb3ecb232bdb1
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:01:25 -0800

    Update Ejercicio29.s

--- Ejercicio29.s
+++ Ejercicio29.s
@@ -4,3 +4,15 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Bucles anidados, operaciones en memoria
+
+# ------------------------------------------------
+/*
+def multiply_matrices(A, B):
+    result = [[0] * len(B[0]) for _ in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+*/
+# -------------------------------------------------

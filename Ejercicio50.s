7e631ba73f87dbaec785cbddcd15d6e3cd3598df
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:19:01 -0800

    Create Ejercicio50.s

---
+++ Ejercicio50.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 50
+# Escribir en un archivo
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Manejo de archivos y llamadas al sistema

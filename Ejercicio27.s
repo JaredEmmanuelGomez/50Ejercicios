bb9993ec3e7285bb0f106903276781d245c10f56
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:02:37 -0800

    Update Ejercicio27.s

--- Ejercicio27.s
+++ Ejercicio27.s
@@ -4,3 +4,10 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Recursión y manejo de memoria
+
+# -------------------------------------------------------------------------------
+/*
+def add_matrices(A, B):
+    return [[A[i][j] + B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
+*/
+# ---------------------------------------------------------------------------------

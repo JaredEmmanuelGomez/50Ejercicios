c729d89f923996e24b6a3f49412a19ae099ffbdf
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 16:37:40 -0800

    Update Ejercicio25.s

--- Ejercicio25.s
+++ Ejercicio25.s
@@ -1,1 +1,6 @@
-
+# Lenguajes de Interfaz - Actividad 25
+# Ordenamiento burbuja
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Algoritmos de ordenamiento

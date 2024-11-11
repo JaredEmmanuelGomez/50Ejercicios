8ddc692d89d699e1c1bf6919fd8576d18f1487d4
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:00:28 -0800

    Update Ejercicio31.s

--- Ejercicio31.s
+++ Ejercicio31.s
@@ -4,3 +4,13 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Análisis de cadenas
+
+# ----------------------------------------------------------------------------------
+/*
+def count_vowels_consonants(string):
+    vowels = 'aeiouAEIOU'
+    v_count = sum(1 for char in string if char in vowels)
+    c_count = sum(1 for char in string if char.isalpha() and char not in vowels)
+    return v_count, c_count
+*/
+# -----------------------------------------------------------------------------------

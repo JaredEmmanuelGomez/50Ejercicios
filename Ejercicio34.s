78e3dc69f63a45271de9b363563d765c22d345b3
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 19:58:38 -0800

    Update Ejercicio34.s

--- Ejercicio34.s
+++ Ejercicio34.s
@@ -4,3 +4,15 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Manipulación de bits
+
+# -----------------------------------------
+/*
+def set_clear_toggle_bits(n, pos, action):
+    if action == "set":
+        return n | (1 << pos)
+    elif action == "clear":
+        return n & ~(1 << pos)
+    elif action == "toggle":
+        return n ^ (1 << pos)
+*/
+# -------------------------------------------

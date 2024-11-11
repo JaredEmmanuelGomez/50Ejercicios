0c23a751c5730f57597ba8512ad6714a576dd506
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 17:12:21 -0800

    Create Ejercicio43.s

---
+++ Ejercicio43.s
@@ -0,0 +1,6 @@
+# Lenguajes de Interfaz - Actividad 43
+# Implementar una cola usando un arreglo
+# Alumno: Gómez Aguilar Jared Emmanuel  
+# Número de Control: 22210309  
+# Python y Ensamblador
+# Estructuras de datos

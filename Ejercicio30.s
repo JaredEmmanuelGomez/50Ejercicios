1157afce4636c29c213dd4c416b25724469ba2f7
SnakeArias <158237071+JaredEmmanuelGomez@users.noreply.github.com>
2024-11-10 20:00:55 -0800

    Update Ejercicio30.s

--- Ejercicio30.s
+++ Ejercicio30.s
@@ -4,3 +4,10 @@
 # Número de Control: 22210309  
 # Python y Ensamblador
 # Manipulación de arreglos
+
+# ------------------------------------------------------------------------------------
+/*
+def transpose_matrix(matrix):
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+*/
+# -------------------------------------------------------------------------------------
